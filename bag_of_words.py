13d891bf698955da731a745b4be025a042f0e29e
robert <robert.otherone@gmail.com>
2018-04-01 13:19:33 -0400

    removed is_json and added term frequencies alongside counts.

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 
 file_name = 'tags.json'
@@ -34,20 +35,35 @@ def get_tags(table):
 def vectorize(data):
     print("vectorize")
     vectorizer = CountVectorizer()
-    print( vectorizer.fit_transform(data).todense() )
-    print( vectorizer.vocabulary_ )
+
+    # This is number of occurences
+    x_tag_counts = vectorizer.fit_transform(data).todense()
+
+    # Prints number of occurences for each social media profile
+    #print( x_tag_counts )
+    #print( vectorizer.vocabulary_ )
+    # Shaping the count
+    x_tag_counts_shape = x_tag_counts.shape
+    print( "Counts" , x_tag_counts_shape )
+
+    # This is number of frequencies (Used to determine for bigger instagram account (bigger accounts have higher count values))
+    # term frequencies
+    # tf_transformer = TfidfTransformer(use_idf=False).fit(x_tag_counts)
+    # x_tag_tf = tf_transformer.transform(x_tag_counts)
+    # x_tag_tf_shape = x_tag_tf.shape
+    # print( "Frequencies", x_tag_tf_shape )
 
 # Change formatting returned from tags
 def format_tags(data):
     print("format tags")
-    
+
     for key, value in data.items():
         print(key)
-    
+
     items_list = data.get('Items')
     #print(type(items_list))
     #print(items_list)
-    
+
     acc_list = []
     for x in items_list:
 
@@ -55,18 +71,11 @@ def format_tags(data):
         str = ""
         for key in x:
             #print (x[key])
-            str += x[key] 
+            str += x[key]
             str += " "
         acc_list.append(str)
+        vectorize(acc_list)
 
-    #print(acc_list)
 
-    vectorize(acc_list)
 
-def is_json(json_str):
-    try:
-        json_object = json.loads(json_str)
-    except ValueError, e:
-        return False
-    return True
 checkExists()
