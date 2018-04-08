777d7412ab09d52524824237ccdf5739c86e5080
root <root@ip-172-31-27-150.us-west-2.compute.internal>
2018-04-08 05:13:57 +0000

    Added NB and SVM

--- bow_classifiers.py
+++ bow_classifiers.py
@@ -3,23 +3,52 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
 df = pd.read_csv('training.csv', sep='\t', names=['Classification', 'Labels'], lineterminator='\n')
+ 
+print df.to_string()
 
-#df.loc[df["Classification"]=='Nature',"Classification"]=0
-#df.loc[df["Classification"]=='Politician',"Classification"]=1
-#df.loc[df["Classification"]=='Space',"Classification"]=2
-#print df.head()
+# Change classifications to numerical values
+df.loc[df["Classification"]=='Nature',"Classification"]=0
+df.loc[df["Classification"]=='Politician',"Classification"]=1
+df.loc[df["Classification"]=='Celebrity',"Classification"]=2
+df.loc[df["Classification"]=='Space',"Classification"]=3
+
+# print out table
 print df.to_string()
 
 df_x=df["Labels"]
 df_y=df["Classification"]
 
 cv = TfidfVectorizer()
-x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
 
-x_traincv= cv.fit_transform(x_train)
+#print df_x
 
-a = x_traincv.toarray()
+x_traincv = cv.fit_transform(df_x)
 
-print a
+x_train_shape = x_traincv.shape
+y_train = df_y.astype('int')
+
+#print y_train
+
+mnb = MultinomialNB()
+mnb.fit(x_traincv, y_train)
+
+#print mnb
+
+test = ['Dark','Night','Space','Nebula','Star','Asteroid','Comet','Sun','Light','Planet']
+
+x_testcv = cv.transform(test)
+
+pred = mnb.predict_proba(x_testcv)
+print "Naive Bayes:"
+print pred
+
+
+svm = SVC(probability=True)
+svm.fit(x_traincv, y_train)
+results = svm.predict_proba(x_testcv)[0]
+#prediction = svm.decision_function(x_testcv)
+print "SVM"
+print results
