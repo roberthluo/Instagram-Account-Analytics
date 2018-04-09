7be6b60ac4a92cffa0c1d90e5c6b158537b1bdc7
John3Kim <johnary_kim@hotmail.com>
2018-04-08 20:50:56 -0400

    Removed unnecessary code and comments

--- analysis.py
+++ analysis.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  8 17:11:55 2018
-
-@author: johnkim
-"""
-
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
 Created on Sun Apr  8 15:32:30 2018
 
 @author: johnkim
@@ -17,24 +9,13 @@ import pandas as pd
 import numpy as np
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from scipy import stats
 
 data_set = pd.read_csv("/Users/johnkim/Documents/GitHub/SEG4300-project/training.csv", 
                        header = None)
-data_set = data_set.replace('\t',' ')
+
+#All the usernames used for each account
 usernames = ['nature','nationalparkservice','narendramodi','realdonaldtrump','justinpjtrudeau','anthony_joshua','kingjames','kendalljenner','therock','kimkardashian','nasa']
-
-words = []
-category = []
-
-for i in range(len(data_set)): 
-    process_row = data_set[0][i].replace('\t',' ').split(' ') 
-    words.append(process_row[:len(process_row)]) 
-    category.append(process_row[0])
-    
-category = pd.Series(category, name = "Category") 
-words = pd.DataFrame(words)
-
 
 # Generate word clouds for each instagram account
 for i in range(len(data_set)): 
@@ -45,9 +26,9 @@ for i in range(len(data_set)):
     plt.title(title)
     plt.imshow(wc)
     plt.show() 
-    plt.imsave(save_img, wc, dpi = 16000)
+    #plt.imsave(save_img, wc, dpi = 16000)
 
-
+# Results from running bow_classifiers
 naive_bayes = pd.Series([ 0.06778083,0.28809941,0.6228622,0.02125756],name = "Naive Bayes")
 svm = pd.Series([0.19072639,0.2441206,0.49359594,0.07155707],name="SVM")
 
@@ -67,4 +48,4 @@ plt.text(0.3,0.2,r_value)
 plt.text(0.3,0.15,equation)
 plt.xlabel("Naive Bayes") 
 plt.ylabel("SVM")
-plt.show()
+plt.show() 
